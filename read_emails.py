f6cc1fc8b07276de9702318cd7eb1d68d29d3b94
Dirk de Wit <dirkdewit@airbears2-10-142-157-83.airbears2.1918.berkeley.edu>
2013-11-08 14:48:04 -0800

    Split the emails to separate files

---
+++ read_emails.py
@@ -0,0 +1,5 @@
+import nltk
+
+output_dir = "/Users/dirkdewit/Documents/School/Master HTI/Internationaal Semester/Applied Natural Language Processing/Assignments/Chatbot/chatbot/testdata"
+
+products = CPCR(output_dir, r'.*\.txt', cat_pattern=r'.*(.{3})\.txt')
