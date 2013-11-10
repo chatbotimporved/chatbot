f7a0e027cd2135c89e87596b83960d9c1161e467
Dirk de Wit <dirkdewit@airbears2-10-142-156-123.airbears2.1918.berkeley.edu>
2013-11-10 15:20:51 -0800

    Updated chunker

--- read_emails.py
+++ read_emails.py
@@ -2,71 +2,57 @@ import nltk
 import random
 import re
 
+from nltk import TaggerI, untag
+from nltk.chunk import ChunkParserI, tree2conlltags
+from nltk.chunk.util import conlltags2tree
+
+from nltk.corpus import conll2000
+from nltk.classify import MaxentClassifier
+
 from nltk.corpus import CategorizedPlaintextCorpusReader as CPCR
 
-class ConsecutiveNPChunkTagger(nltk.TaggerI):
-    def __init__(self, train_sents):
-        train_set = []
-        for tagged_sent in train_sents:
-            untagged_sent = nltk.tag.untag(tagged_sent)
-            history = []
-            for i, (word, tag) in enumerate(tagged_sent):
-                featureset = npchunk_features(untagged_sent, i, history)
-                train_set.append( (featureset, tag) )
-                history.append(tag)
-        self.classifier = nltk.NaiveBayesClassifier.train(train_set)
+class ConsecutiveNPChunkTagger(TaggerI):
+
+    def __init__(self, feature_extractor, classifier):
+        self.feature_extractor = feature_extractor
+        self.classifier = classifier
 
     def tag(self, sentence):
         history = []
+
     	for i, word in enumerate(sentence):
-            featureset = npchunk_features(sentence, i, history)
+            featureset = self.feature_extractor(sentence, i, history)
             tag = self.classifier.classify(featureset)
             history.append(tag)
+
         return zip(sentence, history)
 
-class ConsecutiveNPChunker(nltk.ChunkParserI):
-    def __init__(self, train_sents):
-        tagged_sents = [[((w,t),c) for (w,t,c) in
-                         nltk.chunk.tree2conlltags(sent)]
-                        for sent in train_sents]
-        self.tagger = ConsecutiveNPChunkTagger(tagged_sents)
+    def train(cls, train_sents, feature_extractor, classifier_cls, **kwargs):
+        train_set = []
 
-    def parse(self, sentence):
-        tagged_sents = self.tagger.tag(sentence)
-        conlltags = [(w,t,c) for ((w,t),c) in tagged_sents]
+        for tagged_sent in train_sents:
+            untagged_sent = untag(tagged_sent)
+            history = []
+
+            for i, (word, tag) in enumerate(tagged_sent):
+                featureset = feature_extractor(untagged_sent, i, history)
+                train_set.append((featureset, tag))
+                history.append(tag)
+
+        classifier = classifier_cls.train(train_set, **kwargs)
+        return cls(feature_extractor, classifier)
+
+class ConsecutiveNPChunker(ChunkParserI):
+    def __init__(self, train_sents, *args, **kwargs):
+        tag_sents = [tree2conlltags(sent) for sent in train_sents]
+        train_chunks = [[((w,t),c) for (w,t,c) in sent] for sent in tag_sents]
+        self.tagger = ConsecutiveNPChunkTagger.train(train_chunks, *args, **kwargs)
+
+    def parse(self, tagged_sent):
+        if not tagged_sent: return None
+        chunks = self.tagger.tag(tagged_sent)
+        conlltags = [(w,t,c) for ((w,t),c) in chunks]
         return conlltags2tree(conlltags)
-
-def npchunk_features(sentence, i, history):
-    word, pos = sentence[i]
-
-    if i == 0:
-    	prevword, prevpos = "<START>", "<START>"
-    else:
-    	prevword, prevpos = sentence[i-1]
-
-    if i == len(sentence)-1:
-    	nextword, nextpos = "<END>", "<END>"
-    else:
-    	nextword, nextpos = sentence[i+1]
-
-    return {"pos": pos, 
-    		"word": word,
-    		"prevpos": prevpos,
-    		"nextpos": nextpos,
-    		"prevpos+pos": "%s+%s" % (prevpos, pos),
-    		"pos+nextpos": "%s+%s" % (pos, nextpos),
-    		"tags-since-dt": tags_since_dt(sentence, i)}
-
-def tags_since_dt(sentence, i):
-	tags = set()
-
-	for word, pos in sentence[:i]:
-		if pos == 'DT':
-			tags = set()
-		else:
-			tags.add(pos)
-
-	return '+'.join(sorted(tags))
 
 def extract_categories(filesDir):
 	emails = CPCR(filesDir, r'.*\.txt', cat_pattern=r'(.*)\.txt')
@@ -74,15 +60,14 @@ def extract_categories(filesDir):
 
 def train_chunker(filesDir):
 	emails = extract_categories(filesDir)
-	
+    	
 	sentences = split_raw_to_sentences(emails.raw())
 
 	tokenized_emails = tokenize_sentences(sentences)
-	tagged_emails = tag_sentences(tokenized_emails)
-
-	print tagged_emails
-	# Return the chunker
-	chunker = ConsecutiveNPChunker(tagged_emails)
+	train_sents = tag_sentences(tokenized_emails)
+	
+	chunker = ConsecutiveNPChunker(train_sents, featx, MaxentClassifier, 
+        minlldelta=0.01, max_iter=10)
 
 	return chunker
 
@@ -124,5 +109,6 @@ def test_classifier(filesDir, classifier):
 	print nltk.classify.accuracy(classifier, test_list)
 
 output_dir = "/Users/dirkdewit/Documents/School/Master HTI/Internationaal Semester/Applied Natural Language Processing/Assignments/Chatbot/chatbot/testdata/"
-classifier = train_chunker(output_dir)
+#classifier = train_chunker(output_dir)
+print conll2000.chunked_sents("train.txt")
 
